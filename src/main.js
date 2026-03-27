a02172d0c3783ed887af6dd85f4d3d45b6339080
jjong0 <ujjong39@gmail.com>
2026-03-27 17:35:41 +0900

    feat: 헤더 색 변환

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-import './style.css';
+import "./style.css";
+
+const header = document.querySelector(".utility-bar");
+
+const handleScroll = () => {
+  if (window.scrollY > 785.33) {
+    header.classList.add("is-scrolled");
+  } else {
+    header.classList.remove("is-scrolled");
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
