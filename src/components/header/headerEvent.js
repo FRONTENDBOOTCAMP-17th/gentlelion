0d3b05e3c7fd99f5e833b773261c921ff6bc62d9
jjong0 <ujjong39@gmail.com>
2026-04-02 13:18:09 +0900

    task: 헤더 hover시 li background color

---
+++ src/components/header/headerEvent.js
@@ -0,0 +1,27 @@
+//스크롤 이벤트
+const header = document.querySelector(".utility-bar");
+
+const handleScroll = () => {
+  if (window.scrollY > 785.33) {
+    header.classList.add("is-scrolled");
+  } else {
+    header.classList.remove("is-scrolled");
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
+
+//메뉴 버튼 이벤트
+const btn = document.getElementById("menu-btn");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+
+btn.addEventListener("click", () => {
+  line1.classList.toggle("translate-y-[2.5px]");
+  line1.classList.toggle("rotate-45");
+
+  line2.classList.toggle("-translate-y-[2.5px]");
+  line2.classList.toggle("-rotate-45");
+
+  header.classList.toggle("is-active");
+});
