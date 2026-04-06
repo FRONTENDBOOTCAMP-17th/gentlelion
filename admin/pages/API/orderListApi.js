570436384c5c3e79ed1898aaba4c5a3f97111aaf
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-06 21:06:22 +0900

    FEAT: Order GET 메서드 API 호출 함수 - 토큰값을 비교해 서버에 호출

---
+++ admin/pages/API/orderListApi.js
@@ -0,0 +1,29 @@
+export async function orderAPI(page) {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      console.error("토큰이 없습니다.");
+      return null;
+    }
+
+    const res = await fetch(
+      `https://api.fullstackfamily.com/api/gentlelion/v1/admin/orders?page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("API 요청 실패");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
