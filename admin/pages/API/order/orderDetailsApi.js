d29aff0d0a4293b91d303be43c93b7e341c8675c
jjong0 <ujjong39@gmail.com>
2026-04-13 10:03:08 +0900

    feat: 주문 api wrapper

--- admin/pages/API/order/orderDetailsApi.js
+++ admin/pages/API/order/orderDetailsApi.js
@@ -1,29 +1,3 @@
-export async function orderDetailsAPI(id) {
-    try {
-        const token = localStorage.getItem("token");
+import { get } from "../../../../shareApi/index.js";
 
-        if (!token) {
-            console.error("토큰이 없습니다.");
-            return null;
-        }
-
-        const res = await fetch(
-            `https://api.fullstackfamily.com/api/gentlelion/v1/admin/orders/${id}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        );
-
-        if (!res.ok) {
-            throw new Error("API 요청 실패");
-        }
-
-        const data = await res.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
-}
+export const orderDetailsAPI = (id) => get(`/admin/orders/${id}`);
