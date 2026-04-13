1b8b57e03d3d6cadece753585c866d257da3abb6
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-13 11:40:03 +0900

    feat: 버튼 활성화 - 버튼 클릭에 따라  클래스 hidden 요소 추가 및 제거

--- src/components/profile/handlerProfile.js
+++ src/components/profile/handlerProfile.js
@@ -3,11 +3,13 @@ import { getToken } from "../API/token/getToken";
 import { loadProfile } from "./loadProfile";
 import { loadWishlist } from "./loadWishlist";
 import { goToIndex } from "./goToIndex";
+import { changeLayout } from "./changeLayout";
 
 async function handlerProfile() {
     const data = await getProfileApi(getToken())
     loadProfile(data);
     loadWishlist(getToken());
     goToIndex();
+    changeLayout();
 }
 handlerProfile();
