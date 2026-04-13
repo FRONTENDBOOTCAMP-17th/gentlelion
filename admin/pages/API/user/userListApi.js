70f82fd12644f582e69dbc20d4ca31518ec64b75
jjong0 <ujjong39@gmail.com>
2026-04-13 10:14:24 +0900

    feat: 유저 api wrapper

--- admin/pages/API/user/userListApi.js
+++ admin/pages/API/user/userListApi.js
@@ -1,29 +1,4 @@
-export async function userApi(page, limit = 20) {
-  try {
-    const token = localStorage.getItem("token");
+import { get } from "../../../../shareApi/index.js";
 
-    if (!token) {
-      console.error("토큰이 없습니다.");
-      return null;
-    }
-
-    const res = await fetch(
-      `https://api.fullstackfamily.com/api/gentlelion/v1/admin/users?page=${page}&limit=${limit}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error("API 요청 실패");
-    }
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+export const userApi = (page, limit = 20) =>
+  get(`/admin/users?page=${page}&limit=${limit}`);
