1c5724f8b102c8206d4c30c3f249ec0decda207b
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-15 23:51:11 +0900

    fix: 사용자 포인트 음수값 방지

--- packages/admin/pages/components/userDetails/adjustPoint.js
+++ packages/admin/pages/components/userDetails/adjustPoint.js
@@ -4,6 +4,10 @@ export async function adjustPoint(type, currentUser) {
   const amount = parseInt(document.getElementById("point-input").value);
   if (!amount || amount <= 0) return alert("포인트 금액을 입력해주세요.");
 
+  if (type === "sub" && currentUser.points - amount < 0) {
+    return alert(`차감 가능한 포인트는 최대 ${currentUser.points.toLocaleString()}P 입니다.`);
+  }
+
   const points =
     type === "add" ? currentUser.points + amount : currentUser.points - amount;
 
