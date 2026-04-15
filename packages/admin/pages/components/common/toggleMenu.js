602703f5ca77c4c33907fa5bba2942cc6e2bba7c
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-15 11:28:51 +0900

    fix: 어드민 디자인 변경

--- packages/admin/pages/components/common/toggleMenu.js
+++ packages/admin/pages/components/common/toggleMenu.js
@@ -1,6 +1,9 @@
 export function toggleMenu() {
     const button = document.getElementById("naviMenuButton");
     const menu = document.getElementById("dropdownMenu");
+    if (!button || !menu) {
+        return;
+    }
     button.addEventListener("click", () => {
         menu.classList.toggle("hidden");
     });
